c23be9a6eff0b765f45fcf974617b70e16a9c6e0
ghaash <daniel.silverman@me.com>
2017-09-25 20:47:16 -0700

    added more plates

--- plates.cpp
+++ plates.cpp
@@ -26,6 +26,11 @@ int main()
 	fives = ((((weight % 45) % 35) % 25) % 10) / 5;
 	ones = (((((weight % 45) % 35) % 25) % 10) % 5) / 1;
 
+	/*if elseif else logic
+	 * any variable has to be == 2
+	 * So, just solve the problem for half the weight and apply that set of weights to each side
+	*/
+
 	//out for quarters, dimes, nickels, pennies
 	std::cout << "45's: " << fortyFives << std::endl;
 	std::cout << "35's: " << thirtyFives << std::endl;
@@ -33,7 +38,7 @@ int main()
 	std::cout << "10's: " << tens << std::endl;
 	std::cout << "5's: " << fives << std::endl;
 	std::cout << "1's: " << ones << std::endl;
-	std::count << ".5's: " << halves << std::endl;
+//	std::count << ".5's: " << halves << std::endl;
 
 	return 0;
 }
